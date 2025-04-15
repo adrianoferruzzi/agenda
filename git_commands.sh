7ed048391563b758938c46edf6bcf41a37b1261d
Adriano José Ferruzzi <pt244041@IFSPROOT.PTB>
2025-04-15 12:26:17 -0300

    IFSP

---
+++ git_commands.sh
@@ -0,0 +1,16 @@
+# clone the project
+git clone https://github.com/adrianoferruzzi/agenda.git
+
+# start git
+git init
+
+# add files on project
+# in this case the "." means the current working directory
+git add .
+
+# salve the changes
+git commit . -m "IFSP"
+
+# shows informations about changes
+git status
+
