b9e3ab45f7b467b79b14d1034e7a7a23ea3cccb5
Adriano José Ferruzzi <adriano8891@gmail.com>
2025-04-15 12:53:42 -0300

    IFSP

--- git_commands.sh
+++ git_commands.sh
@@ -1,6 +1,18 @@
 # clone the project
 git clone https://github.com/adrianoferruzzi/agenda.git
 
+# start your github on terminal
+# create ssh keys
+ssh-keygen -t ed25519 -C "email@mail.com"
+# add the public key on github
+# Settings --> SSH and GPG keys 
+# put your public keys
+
+# set yout remote access
+git remote set-url origin git@github.com:yourusername/agenda.git
+
+
+
 # start git
 git init
 
@@ -14,3 +26,14 @@ git commit . -m "IFSP"
 # shows informations about changes
 git status
 
+# view a chronological list of commit history
+git log
+
+# remove file
+git rm file1.txt
+
+# pull files from github and refresh your local project
+git pull main
+
+# push files from your local project to github
+git push agenda
